070b202c025f636f7b29875c20ecbed8c0daa302
Ismaila Hassan <code.ismailah28@gmail.com>
2025-05-04 00:47:35 +0100

    feat: add auth status and logout functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-200 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,7 @@
         <p class="py-6">
           Keep trak of your travels and adventures with this simple travel log app. Add locations, places and notes to create a digital journal of your journeys.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
       </div>
     </div>
   </div>
