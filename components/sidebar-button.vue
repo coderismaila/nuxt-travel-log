33c2328a54b9188cfc176fb88bbb36816a50f968
Ismaila Hassan <code.ismailah28@gmail.com>
2025-05-04 11:40:33 +0100

    feat: add sidebar with persistent collapsed tate

---
+++ components/sidebar-button.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  showLabel: boolean;
+  label: string;
+  icon: string;
+  href: string;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div
+    class="tooltip-right"
+    :data-tip="showLabel ? undefined : label"
+    :class="{ tooltip: !showLabel }"
+  >
+    <NuxtLink
+      :to="href"
+      :class="{ 'bg-base-200': route.path === props.href, 'justify-start': showLabel, 'justify-center': !showLabel }"
+      class="flex gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap"
+    >
+      <Icon :name="icon" size="24" />
+      <Transition name="grow">
+        <span v-if="showLabel">
+          {{ label }}
+        </span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1;
+}
+.grow-leave-active {
+  animation: grow 0.1 reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
