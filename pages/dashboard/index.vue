ec06d6b168a40a6590117304153b4245c434992b
Ismaila Hassan <87364949+coderismaila@users.noreply.github.com>
2025-05-04 11:47:02 +0100

    feat: add sidebar with persistent collapsed state (#59)

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,3 +1,54 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = JSON.parse(localStorage.getItem("isSidebarOpen") || "true");
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", JSON.stringify(isSidebarOpen.value));
+}
+</script>
+
 <template>
-  <div>Dashboard</div>
+  <div class="flex-1 flex">
+    <div
+      class="bg-base-100 transition-all duration-300"
+      :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }"
+    >
+      <div
+        class="flex hover:cursor-pointer hover:bg-base-200 p-2"
+        :class="{ 'justify-end': isSidebarOpen, 'justify-center': !isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon
+          :name="isSidebarOpen ? 'tabler:chevron-left' : 'tabler:chevron-right'"
+          size="32"
+        />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton
+          href="/dashboard/locations"
+          :show-label="isSidebarOpen"
+          label="Locations"
+          icon="tabler:map"
+        />
+        <SidebarButton
+          href="/locations"
+          :show-label="isSidebarOpen"
+          label="Add Location"
+          icon="tabler:circle-plus-filled"
+        />
+        <div class="divider" />
+        <SidebarButton
+          href="/sign-out"
+          :show-label="isSidebarOpen"
+          label="Sign Out"
+          icon="tabler:logout-2"
+        />
+      </div>
+    </div>
+    <div class="flex-1 bg-amber-200" />
+  </div>
 </template>
