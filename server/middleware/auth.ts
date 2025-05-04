02d019fd674e403a71af7ef1428c491ee9c532b0
Ismaila Hassan <code.ismailah28@gmail.com>
2025-05-04 02:03:36 +0100

    feat: add SSR auth

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,7 +3,7 @@ import { auth } from "~/lib/auth";
 export default defineEventHandler(async (event) => {
   if (event.path.startsWith("/dashboard")) {
     const session = await auth.api.getSession({ headers: event.headers });
-    if (!session) {
+    if (!session?.user) {
       sendRedirect(event, "/", 302);
     }
   }
