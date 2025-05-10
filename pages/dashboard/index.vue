3f72633182ffcc32521db299b55ec458c5763a83
Ismaila Hassan <code.ismailah28@gmail.com>
2025-05-10 12:24:07 +0100

    feat: show location detail after clicking marker

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+const mapStore = useMapStore();
 const locationsStore = useLocationStore();
 const { locations, status } = storeToRefs(locationsStore);
 
@@ -20,7 +21,13 @@ onMounted(() => {
       <div
         v-for="location in locations"
         :key="location.id"
-        class="card card-compact bg-base-300 h-40 w-72 shrink-0"
+        class="card card-compact bg-base-300 h-40 w-72 mb-2 border-2 shrink-0 hover:cursor-pointer"
+        :class="{
+          'border-accent': location === mapStore.selectedPoint,
+          'border-transparent': location !== mapStore.selectedPoint,
+        }"
+        @mouseenter="mapStore.selectedPoint = location"
+        @mouseleave="mapStore.selectedPoint = null"
       >
         <div class="card-body">
           <h3 class="text-xl">
